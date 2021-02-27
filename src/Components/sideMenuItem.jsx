15f0e3a8e0fe718b379b9bbc6044b7522ebeb963
Jerry <jerryhamada@berkeley.edu>
2021-02-27 00:38:00 -0800

    simplified code

---
+++ src/Components/sideMenuItem.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Menu } from "semantic-ui-react";
+
+import { Typography, Grid } from "@material-ui/core";
+
+const SideMenuObj = (props) => {
+  return (
+    <>
+      <Menu.Item onClick={() => {}}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid item xs={2}>
+            {props.icon}
+          </Grid>
+          <Grid item m>
+            <Typography variant="h6" children={props.name}></Typography>
+          </Grid>
+        </Grid>
+      </Menu.Item>
+    </>
+  );
+};
+
+export default SideMenuObj;
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -3,15 +3,18 @@ import { Typography, Grid } from "@material-ui/core";
 import { RiUser3Line } from "react-icons/ri";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { BsListUl } from "react-icons/bs";
-import { FiCreditCard } from "react-icons/fi";
+import { FiCreditCard, FiLogOut } from "react-icons/fi";
 import { AiOutlineBank } from "react-icons/ai";
+import { IoMdHelp } from "react-icons/io";
+import { MdLanguage } from "react-icons/md";
 
 import "fontsource-roboto";
 
-import { Menu, Sidebar, Divider } from "semantic-ui-react";
+import { Menu, Sidebar, Divider, Container } from "semantic-ui-react";
+import SideMenuObj from "./sideMenuItem";
 
 class Sidenav extends Component {
-  state = { activeItem: "profile" };
+  state = { activeItem: null };
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
   render() {
@@ -21,6 +24,8 @@ class Sidenav extends Component {
         <Sidebar.Pushable>
           <Sidebar
             as={Menu}
+            size="huge"
+            borderless
             animation="overlay"
             icon="labeled"
             onHide={this.props.onHideSidebar}
@@ -28,148 +33,49 @@ class Sidenav extends Component {
             visible={this.props.visible}
             direction="right"
           >
-            <Menu text vertical borderless size="huge">
-              <Menu.Item
-                name="profile"
-                onClick={this.handleItemClick}
-                active={activeItem === "profile"}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item xs={3}>
-                    <RiUser3Line size={25} />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" children="Profile"></Typography>
-                  </Grid>
-                </Grid>
-              </Menu.Item>
-              <Menu.Item
-                name="bookmarks"
-                onClick={this.handleItemClick}
-                active={activeItem === "bookmarks"}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item xs={3}>
-                    <IoBookmarkOutline size={25} />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" children="Bookmarks"></Typography>
-                  </Grid>
-                </Grid>
-              </Menu.Item>
-              <Menu.Item
-                name="Lists"
-                onClick={this.handleItemClick}
-                active={activeItem === "Lists"}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item xs={3}>
-                    <BsListUl size={25} />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" children="Lists"></Typography>
-                  </Grid>
-                </Grid>
-              </Menu.Item>
+            <SideMenuObj
+              name="Profile"
+              icon={<RiUser3Line size={25} />}
+            ></SideMenuObj>
+            <SideMenuObj
+              name="Bookmarks"
+              icon={<IoBookmarkOutline size={25} />}
+            ></SideMenuObj>
+            <SideMenuObj
+              name="Lists"
+              icon={<BsListUl size={25} />}
+            ></SideMenuObj>
 
-              <Divider clearing />
+            <Divider clearing />
 
-              <Menu.Item
-                name="Your Cards"
-                onClick={this.handleItemClick}
-                active={activeItem === "Your Cards"}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item xs={3}>
-                    <FiCreditCard size={25} />
-                  </Grid>
-                  <Grid item>
-                    <Typography
-                      variant="h6"
-                      children="Your Cards (to subscribe)"
-                    ></Typography>
-                  </Grid>
-                </Grid>
-              </Menu.Item>
-              <Menu.Item
-                name="Add Bank"
-                onClick={this.handleItemClick}
-                active={activeItem === "Add Bank"}
-              >
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item xs={3}>
-                    <AiOutlineBank size={30} />
-                  </Grid>
-                  <Grid item>
-                    <Typography
-                      variant="h6"
-                      children="Add bank (to earn)"
-                    ></Typography>
-                  </Grid>
-                </Grid>
-              </Menu.Item>
+            <SideMenuObj
+              name="Your Cards (to subscribe)"
+              icon={<FiCreditCard size={25} />}
+            ></SideMenuObj>
+            <SideMenuObj
+              name="Add Bank"
+              icon={<AiOutlineBank size={30} />}
+            ></SideMenuObj>
 
-              <Divider clearing />
+            <Divider clearing />
 
-              <Menu.Item
-                name="Help"
-                onClick={this.handleItemClick}
-                active={activeItem === "Help"}
-              >
-                <Typography
-                  variant="h6"
-                  children="Help and Support"
-                ></Typography>
-              </Menu.Item>
-              <Menu.Item
-                name="Dark Mode"
-                onClick={this.handleItemClick}
-                active={activeItem === "Dark mode"}
-              >
-                <Typography variant="h6" children="Dark Mode"></Typography>
-              </Menu.Item>
-              <Menu.Item
-                name="English"
-                onClick={this.handleItemClick}
-                active={activeItem === "English"}
-              >
-                <Typography variant="h6" children="English"></Typography>
-              </Menu.Item>
+            <SideMenuObj
+              name="Help and Support"
+              icon={<IoMdHelp size={25} />}
+            ></SideMenuObj>
+            <SideMenuObj
+              name="English"
+              icon={<MdLanguage size={25} />}
+            ></SideMenuObj>
 
-              <Divider clearing />
-              <Menu.Item
-                name="Log Out"
-                onClick={this.handleItemClick}
-                active={activeItem === "Log Out"}
-              >
-                <Typography variant="h6" children="Log Out"></Typography>
-              </Menu.Item>
-            </Menu>
+            <Divider clearing />
+
+            <SideMenuObj
+              name="Log Out"
+              icon={<FiLogOut size={25} />}
+            ></SideMenuObj>
+
+            <Menu text vertical size="huge"></Menu>
           </Sidebar>
 
           <Sidebar.Pusher dimmed={this.props.visible}></Sidebar.Pusher>
