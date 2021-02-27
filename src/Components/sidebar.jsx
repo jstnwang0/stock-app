f8de45fadae524ae48005220c7f12ce0672522f5
Jerry <jerryhamada@berkeley.edu>
2021-02-26 16:30:57 -0800

    added the side bar

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
-import { Icon, Menu, Sidebar } from "semantic-ui-react";
+import {
+  Button,
+  Container,
+  Icon,
+  Menu,
+  Sidebar,
+  Header,
+  Divider,
+} from "semantic-ui-react";
+
+// import { Button } from "react-bootstrap";
+
+// import { Button } from "react-bootstrap";
 
 class Sidenav extends Component {
   render() {
@@ -11,25 +23,93 @@ class Sidenav extends Component {
             as={Menu}
             animation="overlay"
             icon="labeled"
-            inverted
             onHide={this.props.onHideSidebar}
             vertical
             visible={this.props.visible}
             direction="right"
-            width="thin"
+            width="wide"
           >
-            <Menu.Item as="a">
-              <Icon name="home" />
-              Home
-            </Menu.Item>
-            <Menu.Item as="a">
-              <Icon name="gamepad" />
-              Games
-            </Menu.Item>
-            <Menu.Item as="a">
-              <Icon name="camera" />
-              Channels
-            </Menu.Item>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="user outline" circular={true} />
+                  My profile
+                </Header>
+              </Button>
+            </Container>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="bookmark outline" circular={true} />
+                  Bookmarks
+                </Header>
+              </Button>
+            </Container>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="list" circular={true} />
+                  Lists
+                </Header>
+              </Button>
+            </Container>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="setting" circular={true} />
+                  Settings
+                </Header>
+              </Button>
+            </Container>
+
+            <Divider clearing />
+
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="credit card outline" circular={true} />
+                  Your Cards (to subscribe)
+                </Header>
+              </Button>
+            </Container>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="money bill alternate outline" circular={true} />
+                  Add Bank (to earn)
+                </Header>
+              </Button>
+            </Container>
+
+            <Divider clearing />
+
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="help" circular={true} />
+                  Help and Support
+                </Header>
+              </Button>
+            </Container>
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="moon outline" circular={true} />
+                  Dark Mode
+                </Header>
+              </Button>
+            </Container>
+
+            <Divider clearing />
+
+            <Container>
+              <Button size="tiny">
+                <Header textAlign="left">
+                  <Icon name="log out" circular={true} />
+                  Log Out
+                </Header>
+              </Button>
+            </Container>
           </Sidebar>
 
           <Sidebar.Pusher dimmed={this.props.visible}></Sidebar.Pusher>
