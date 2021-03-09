2f07a1e5948a70efa7014401013cbe7804da8423
Jerry <jerryhamada@berkeley.edu>
2021-03-08 18:50:06 -0800

    deleted unnecessary shit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./Components/navigationBar";
 import NaviBar from "./Components/navigationBar";
---
+++ src/NavigationBar/navigationBar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { AiOutlineHome, AiOutlineBell } from "react-icons/ai";
+import { BiMessageAltAdd } from "react-icons/bi";
+import { FaRegCommentAlt } from "react-icons/fa";
+import { RiUser3Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const NaviBar = ({ activeItem, onItemClick, onSidebarClick }) => {
+  return (
+    <div
+    //  className="footer"
+    >
+      <Menu
+        style={{ boxShadow: "none" }}
+        borderless
+        fixed="top"
+        fluid
+        widths={5}
+      >
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          color={activeItem === "home" ? "green" : null}
+          onClick={onItemClick}
+          as={Link}
+          to="/"
+        >
+          <AiOutlineHome size={25} />
+        </Menu.Item>
+        <Menu.Item
+          name="notifications"
+          active={activeItem === "notifications"}
+          color={activeItem === "notifications" ? "green" : null}
+          onClick={onItemClick}
+          as={Link}
+          to="/notifications"
+        >
+          <AiOutlineBell size={25} />
+        </Menu.Item>
+        <Menu.Item
+          name="createPost"
+          active={activeItem === "createPost"}
+          color={activeItem === "createPost" ? "green" : null}
+          onClick={onItemClick}
+          as={Link}
+          to="/createPost"
+        >
+          <BiMessageAltAdd size={25} />
+        </Menu.Item>
+        <Menu.Item
+          name="chat"
+          active={activeItem === "chat"}
+          color={activeItem === "chat" ? "green" : null}
+          onClick={onItemClick}
+          as={Link}
+          to="/chat"
+        >
+          <FaRegCommentAlt size={25} />
+        </Menu.Item>
+        <Menu.Item name="profile" onClick={onSidebarClick}>
+          <RiUser3Line size={25} />
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+export default NaviBar;
