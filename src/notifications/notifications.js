cf3ea04b88a9ec5d5947cc044962172cd3180203
Jerry <jerryhamada@berkeley.edu>
2021-03-11 16:18:12 -0800

    just did notifications first draft

--- src/notifications/notifications.js
+++ src/notifications/notifications.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./notifications.css";
+import { Avatar } from "@material-ui/core";
+import NotificationData from "../data/notifications.json";
 
 function Notifications() {
+  var notificationList = NotificationData.notifications;
   return (
-    <div>
-      <h3>Notifications</h3>
-    </div>
+    <ul>
+      {notificationList.map((s) => {
+        return (
+          <div className="main-box">
+            <div className="avatar">
+              <Avatar src={s.avatar} />
+            </div>
+            <div className="message-box">
+              @{s.username} <p>{s.message}</p>
+            </div>
+            <div className="time-stamp">{s.time}</div>
+          </div>
+        );
+      })}
+    </ul>
   );
 }
 
